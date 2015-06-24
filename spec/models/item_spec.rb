4af13d289a9fd336e0c6e415dd50f3478e52b4a2
Rosemary Thomas <rsm.thomas@gmail.com>
2015-06-24 16:27:24 -0600

    fixed item model order test.

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  let(:item_1) {Item.create(name: 'studio')}
-  let(:item_2) {Item.create(name: 'house')}
-  let(:item_3) {Item.create(name: 'office')}
+  let(:item_1) {Item.create(name: 'studio',description: 'stuff', price: 'money')}
+  let(:item_2) {Item.create(name: 'house',description: 'stuff', price: 'money')}
+  let(:item_3) {Item.create(name: 'office',description: 'stuff', price: 'money')}
   
   describe "items" do
     it 'order items by name' do
@@ -13,4 +13,5 @@ RSpec.describe Item, type: :model do
       expect(Item.all.by_name.first).to eq(item_2)
     end
   end
+
 end
