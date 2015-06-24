1f786cd4cc61f91dbdd3a6edce9021e5dace5a60
Rosemary Thomas <rsm.thomas@gmail.com>
2015-06-24 13:40:34 -0600

    category spec successful

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  let(:category) {@category_1 = Category.create!(name: 'housing')}
-  let(:category) {@category_2 = Category.create!(name: 'jobs')}
-  let(:category) {@category_3 = Category.create!(name: 'for sale')}
-
+  before(:each) do
+    @category_1 = Category.create(name: 'housing')
+    @category_2 = Category.create(name: 'jobs')
+    @category_3 = Category.create(name: 'for sale')
+  end
+  
   describe "categories" do
-    binding.pry
     it 'order categories by name' do
       expect(Category.all.by_name.first).to eq(@category_3)
     end
