1d5d7a6f32ec67a4031d5f0925a0841ae48ecb97
Rosemary Thomas <rsm.thomas@gmail.com>
2015-06-24 13:33:49 -0600

    started ordering categories spec, added pry gem for debugging

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
 class Category < ActiveRecord::Base
+
+  def self.by_name
+    order(:name)
+  end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:category) {@category_1 = Category.create!(name: 'housing')}
+  let(:category) {@category_2 = Category.create!(name: 'jobs')}
+  let(:category) {@category_3 = Category.create!(name: 'for sale')}
+
+  describe "categories" do
+    binding.pry
+    it 'order categories by name' do
+      expect(Category.all.by_name.first).to eq(@category_3)
+    end
+  end
+
 end
