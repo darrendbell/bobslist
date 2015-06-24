6b52a25045b7f11418a1df7a19944cf0563b18cd
Rosemary Thomas <rsm.thomas@gmail.com>
2015-06-24 13:51:42 -0600

    created items controller with index, edit, show, and update views.

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,13 @@
+class ItemsController < ApplicationController
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'items/index'
+
+  get 'items/edit'
+
+  get 'items/update'
+
+  get 'items/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
